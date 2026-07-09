59c97c5191c17d9833994cceedf0fb232925306f
Fernando Lugo <fernandolugo@MacBook-Air-de-Fernando.local>
2026-07-09 16:13:42 -0500

    [FIX] Quitar --remote-debugging-port fijo, choca con la gestion interna de ChromeDriver
    Causaba "DevToolsActivePort file doesn't exist" aunque Chromium arrancaba bien

--- src/abilities/browse_the_web.py
+++ src/abilities/browse_the_web.py
@@ -19,7 +19,6 @@ class BrowseTheWeb:
                 chrome_options.add_argument("--no-sandbox")
                 chrome_options.add_argument("--disable-dev-shm-usage")
                 chrome_options.add_argument("--disable-gpu")
-                chrome_options.add_argument("--remote-debugging-port=9222")
                 chrome_options.binary_location = os.environ.get("CHROME_BIN", "/usr/bin/chromium")
                 service = Service(os.environ.get("CHROMEDRIVER_BIN", "/usr/bin/chromedriver"))
             else:
